91590d4ed8d26b6c3276e4566064c40099314fc4
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-11-30 19:05:09 +0500

    restructured remap.lua

--- .config/nvim/lua/sj/remap.lua
+++ .config/nvim/lua/sj/remap.lua
@@ -1,30 +1,30 @@
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap("n", "<leader>pv", ":Ex<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 
-vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = false })
-vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = false })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = false })
-vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "<leader>Y", '"+Y', { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>s",
 	":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
 	{ noremap = true, silent = false }
 )
 
-vim.api.nvim_set_keymap("n", "<leader>ut", ":UndotreeToggle<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>ut", ":UndotreeToggle<CR>")
 
-vim.api.nvim_set_keymap("n", "J", "mzJ`z", { noremap = true, silent = false })
+vim.keymap.set("n", "J", "mzJ`z")
 
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = false })
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = false })
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.api.nvim_set_keymap("x", "<leader>pd", '"_dP', { noremap = true, silent = false })
+vim.keymap.set("x", "<leader>pd", '"_dP')
 
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
@@ -35,3 +35,5 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga hover_doc <CR>")
 vim.keymap.set("n", "<A-t>", "<cmd>Lspsaga term_toggle <CR>")
+
+vim.keymap.set("n", "<A-s>", "<cmd>CloakToggle <CR>")
