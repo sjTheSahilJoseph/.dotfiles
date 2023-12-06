ef996cbff711ae3476d3f8c569ed9f7018cf3df0
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-12-06 20:20:37 +0500

    fix: some stuff

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,20 +1,55 @@
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({ buffer = bufnr })
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "<leader>gD", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vfc", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = {
-
-        "lua_ls"
+    ensure_installed = {
+        "pyright", "lua_ls"
     },
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
 require("lspconfig").lua_ls.setup({})
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+    },
+    formatting = lsp_zero.cmp_format(),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
+})
+vim.keymap.set({ "i" }, "<C-d>", function() ls.expand() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-l>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-e>", function()
+    if ls.choice_active() then
+        ls.change_choice(1)
+    end
+end, { silent = true })
--- .config/nvim/lua/sj/remap.lua
+++ .config/nvim/lua/sj/remap.lua
@@ -1,58 +1,13 @@
 vim.g.mapleader = " "
-
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
-
-vim.keymap.set(
-	"n",
-	"<leader>s",
-	":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
-	{ noremap = true, silent = false }
-)
-
-vim.keymap.set("n", "<leader>ut", ":UndotreeToggle<CR>")
-
+vim.keymap.set("n", "<leader>s",":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "J", "mzJ`z")
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
 vim.keymap.set("x", "<leader>pd", '"_dP')
-
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
-vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
-vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
-vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {})
-
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
-vim.keymap.set("n", "<A-s>", "<cmd>CloakToggle <CR>")
-
-vim.keymap.set("n", "<A-z>", "<cmd>ZenMode <CR>")
-
--- Lua
-vim.keymap.set("n", "<leader>xx", function()
-	require("trouble").toggle()
-end)
-vim.keymap.set("n", "<leader>xw", function()
-	require("trouble").toggle("workspace_diagnostics")
-end)
-vim.keymap.set("n", "<leader>xd", function()
-	require("trouble").toggle("document_diagnostics")
-end)
-vim.keymap.set("n", "<leader>xq", function()
-	require("trouble").toggle("quickfix")
-end)
-vim.keymap.set("n", "<leader>xl", function()
-	require("trouble").toggle("loclist")
-end)
-vim.keymap.set("n", "gR", function()
-	require("trouble").toggle("lsp_references")
-end)
