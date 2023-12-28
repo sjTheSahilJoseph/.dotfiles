666bdf7fd61e669d91fbc9dda29c8bac80eee6d5
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-12-28 18:35:39 +0500

    some stuff

--- .config/nvim/lua/sj/set.lua
+++ .config/nvim/lua/sj/set.lua
@@ -1,16 +1,19 @@
+vim.g.mapleader = " "
+
 vim.opt.guicursor = ""
+
+vim.opt.mouse = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+vim.opt.wrap = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
-vim.opt.wrap = false
 
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
@@ -21,11 +24,6 @@ vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 
 vim.opt.updatetime = 50
-
--- vim.opt.spelllang = "en"
--- vim.opt.spell = true
-
-vim.g.mapleader = " "
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -38,7 +36,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
 
-vim.opt.mouse = ""
 
 
 
