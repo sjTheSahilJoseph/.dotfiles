b5c15101c6a762ef693f617a385f69455b29c62b
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-01-14 11:57:24 +0500

    customized tmux_session

--- .config/Scripts/tmux_session.sh
+++ .config/Scripts/tmux_session.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
@@ -10,16 +10,9 @@ if [[ -z $selected ]]; then
     exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
-
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
+if [[ -z $TMUX ]]; then
+    tmux new-session -c $selected
+else
+    tmux new-session -d -c $selected
+    tmux switch-client -n
 fi
-
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
-fi
-
-tmux switch-client -t $selected_name
