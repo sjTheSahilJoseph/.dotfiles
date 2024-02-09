fa3199ce64c189c0782305325ad26e42b696d859
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-09 13:43:18 +0500

    setLcdResolution.sh: fixed

--- .config/Scripts/setLcdResolution.sh
+++ .config/Scripts/setLcdResolution.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mode=$(cvt 1920 1080 | grep Modeline | cut -d' ' -f2-)
+cvt 1920 1080
 
-xrandr --newmode $mode
+xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 
 xrandr --addmode VGA-1 "1920x1080_60.00"
 
