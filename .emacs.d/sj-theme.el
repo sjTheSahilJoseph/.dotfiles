ab6b12bd606b8c37c2890774562c6dae3200b943
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-01 10:19:28 +0500

    ...

---
+++ .emacs.d/sj-theme.el
@@ -0,0 +1,13 @@
+(deftheme sj
+  "Created 2024-03-31.")
+
+(custom-theme-set-variables
+ 'sj
+ '(ispell-dictionary nil)
+ '(blink-cursor-mode nil))
+
+(custom-theme-set-faces
+ 'sj
+ '(default ((t (:slant normal :weight semi-bold :height 113 :width normal :foundry "CYRE" :family "Inconsolata")))))
+
+(provide-theme 'sj)
