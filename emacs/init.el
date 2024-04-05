54a3e10347e2f5bf56baf0fe43480745c277a64b
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-05 22:11:05 +0500

    ...

--- emacs/init.el
+++ emacs/init.el
@@ -3,4 +3,24 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (global-hl-line-mode t)
-(set-default-font "Ubuntu Mono-18)
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "DejaVu Sans Mono"
+                    :height 120
+                    :weight 'normal
+                    :width 'normal)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(manoj-dark))
+ '(ispell-dictionary nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/loveisanillusion.emacs-theme.el
@@ -0,0 +1,40 @@
+(deftheme loveisanillusion.emacs
+  "Created 2024-04-02.")
+(custom-theme-set-faces
+ 'loveisanillusion.emacs
+ ;; Default text color
+ '(default ((t (:foreground "#b1b1b1" :background "#000000"))))
+ ;; Diff faces
+ '(diff-changed ((t (:foreground "#cec9c9"))))
+ '(diff-added ((t (:foreground "#f39207"))))
+ '(diff-removed ((t (:foreground "#cec9c9"))))
+ ;; Error and warning messages
+ '(error ((t (:foreground "#cec9c9"))))
+ '(warning ((t (:foreground "#cec9c9"))))
+ ;; Preprocessor directives
+ '(font-lock-preprocessor-face ((t (:foreground "#cec9c9"))))
+ ;; Constants and literals
+ '(font-lock-constant-face ((t (:foreground "#f39207"))))
+ ;; Keywords
+ '(font-lock-keyword-face ((t (:foreground "#f39207"))))
+ ;; Strings
+ '(font-lock-string-face ((t (:foreground "#21eb10"))))
+ ;; Functions
+ '(font-lock-function-name-face ((t (:foreground "#f6f7f8"))))
+ ;; Line numbers
+ '(line-number ((t (:foreground "#4b4b4b" :background "#000000"))))
+ ;; Comments
+ '(font-lock-comment-face ((t (:foreground "#096dc1" :slant italic))))
+ '(font-lock-comment-delimiter-face ((t (:foreground "#096dc1" :slant italic))))
+ ;; Mode line
+ '(mode-line ((t (:bold t :foreground "#b1b1b1" :background "#181717"))))
+ '(mode-line-inactive ((t (:foreground "#b1b1b1" :background "#000000"))))
+ ;; Search highlight
+ '(isearch ((t (:background "#6a6a6d" :foreground "#b1b1b1"))))
+ ;; Vertical split
+ '(vertical-border ((t (:foreground "#181717"))))
+ ;; Search match
+ '(match ((t (:background "#540408"))))
+ )
+
+(provide-theme 'loveisanillusion.emacs)
