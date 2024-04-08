aecd54892f8e213110193a879f78b0606376c28d
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-08 22:14:05 +0500

    Kitty: Enabled Shell Integration ... for Cursor

--- nvim/lua/sj/packer.lua
+++ nvim/lua/sj/packer.lua
@@ -6,7 +6,6 @@ return require("packer").startup(function(use)
         "nvim-telescope/telescope.nvim",
         requires = { { "nvim-lua/plenary.nvim" } },
     })
-    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
     use("tpope/vim-fugitive")
     use("mbbill/undotree")
     use("laytan/cloak.nvim")
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -134,11 +134,6 @@ _G.packer_plugins = {
     path = "/home/sj/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
-  ["nvim-treesitter"] = {
-    loaded = true,
-    path = "/home/sj/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
-    url = "https://github.com/nvim-treesitter/nvim-treesitter"
-  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/sj/.local/share/nvim/site/pack/packer/start/packer.nvim",
