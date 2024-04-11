d2baddb801eca08ea117536ff4d6e2073359c36e
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-11 15:00:03 +0500

    NeoVim: ...

--- nvim/lua/sj/set.lua
+++ nvim/lua/sj/set.lua
@@ -4,15 +4,15 @@ vim.opt.guicursor = 'n-v-c:ver20,i-ci-ve:ver20,r-cr:ver20,o:ver20,sm:ver20'
 
 vim.opt.mouse = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.nu = false
+vim.opt.relativenumber = false
 
 vim.opt.wrap = false
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = false
 vim.opt.smartindent = true
 
 vim.opt.incsearch = true
