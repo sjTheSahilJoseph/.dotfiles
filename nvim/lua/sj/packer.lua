d0c29a3ac628f0c76b4b81336562f7d54b2443d8
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-11 15:24:05 +0500

    neovim: setup treesitter and some config

--- nvim/lua/sj/packer.lua
+++ nvim/lua/sj/packer.lua
@@ -29,6 +29,8 @@ return require("packer").startup(function(use)
         {'hrsh7th/nvim-cmp'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'L3MON4D3/LuaSnip'},
-      }
-}
+	      }
+	}
+
+	use 'nvim-treesitter/nvim-treesitter'
 end)
