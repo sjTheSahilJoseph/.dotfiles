7d2ca5087a14a403861bc95dfaf409cf28950e08
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-11 15:00:15 +0500

    Emacs: ...

--- emacs/init.el
+++ emacs/init.el
@@ -24,3 +24,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(scroll-bar-mode -1)
+(setq-default cursor-type 'bar)
+(set-cursor-color "#26d726")
+(global-hl-line-mode -1)
+
+
+(setq window-divider-default-right-width 0) ; Adjust the value as needed
+(setq window-divider-default-left-width 0)
+(setq window-divider-default-left-width 0)
+(setq window-divider-default-bottom-width 0) ; Adjust the value as needed
+
+;; Enable the window divider mode
+(window-divider-mode)
+
+;; Set the internal border width (space around text area within the frame)
+(setq default-frame-alist '((internal-border-width . 2))) ; Adjust the value as needed
+
+;; Set the external border width (space between frame and window manager's decorations)
+(setq default-frame-alist '((left-fringe . 5) (right-fringe . 5))) ; Adjust the value as needed
