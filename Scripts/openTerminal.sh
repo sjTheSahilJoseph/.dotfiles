5821fda76bc8b431a052ce6d077a8f8da80089a2
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-21 12:14:30 +0500

    Terminal, Tmux, Sessions Setup

--- Scripts/openTerminal.sh
+++ Scripts/openTerminal.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 
-for i in {1..3}; do
-    session="s$i"
+    session="learning"
+    tmux new-session -d -s $session -n "english"
+    tmux new-window -t $session: -n "computer"
+    tmux new-window -t $session: -n "mathematics"
+    tmux new-window -t $session: -n "physics"
+    tmux new-window -t $session: -n "files"
+    tmux new-window -t $session: -n "temp"
+
+    session="work1"
     tmux new-session -d -s $session -n "vim"
     tmux new-window -t $session: -n "run"
     tmux new-window -t $session: -n "auxiliary"
     tmux new-window -t $session: -n "music"
+    tmux new-window -t $session: -n "files"
     tmux new-window -t $session: -n "temp"
-done
 
-tmux attach-session -t s1
+    session="work2"
+    tmux new-session -d -s $session -n "vim"
+    tmux new-window -t $session: -n "run"
+    tmux new-window -t $session: -n "auxiliary"
+    tmux new-window -t $session: -n "music"
+    tmux new-window -t $session: -n "files"
+    tmux new-window -t $session: -n "temp"
+
+    session="work3"
+    tmux new-session -d -s $session -n "vim"
+    tmux new-window -t $session: -n "run"
+    tmux new-window -t $session: -n "auxiliary"
+    tmux new-window -t $session: -n "music"
+    tmux new-window -t $session: -n "files"
+    tmux new-window -t $session: -n "temp"
+
+    session="teaching"
+    tmux new-session -d -s $session -n "vim"
+    tmux new-window -t $session: -n "run"
+    tmux new-window -t $session: -n "auxiliary"
+    tmux new-window -t $session: -n "music"
+    tmux new-window -t $session: -n "files"
+    tmux new-window -t $session: -n "temp"
+
+
+tmux attach-session -t learning
