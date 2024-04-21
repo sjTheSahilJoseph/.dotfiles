6e77e759616c86e55ca9115b0531ee6515dedbec
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-21 07:59:07 +0500

    Session: Window Name

--- Scripts/openTerminal.sh
+++ Scripts/openTerminal.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 
 for i in {1..3}; do
     session="s$i"
-    tmux new-session -d -s $session -n "nvim"
+    tmux new-session -d -s $session -n "vim"
     tmux new-window -t $session: -n "run"
     tmux new-window -t $session: -n "auxiliary"
     tmux new-window -t $session: -n "music"
