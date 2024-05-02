919da882dd0e722f3e9d711a47a0332a6b430a0f
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-02 07:36:26 -0700

    Emacs: Theme setting

--- emacs/loveisanillusion.emacs-theme.el
+++ emacs/loveisanillusion.emacs-theme.el
@@ -1,44 +1,65 @@
 
-(deftheme loveisanillusion.emacs
-  "Created 2024-04-02.")
+(deftheme loveisanillusion.emacs "Custom Emacs Theme")
+
+(let ((color_1 "#cec9c9")
+      (color_2 "#000000")
+      (color_3 "#f39207")
+      (color_4 "#21eb10")
+      (color_5 "#00acff")
+      (color_6 "#540408")
+      (color_7 "#f05624")
+      (color_8 "#b1b1b1")
+      (color_9 "#181717")
+      )
+
 (custom-theme-set-faces
  'loveisanillusion.emacs
- ;; Default text color
- '(default ((t (:foreground "#b1b1b1" :background "#000000"))))
- ;; Diff faces
- '(diff-changed ((t (:foreground "#cec9c9"))))
- '(diff-added ((t (:foreground "#f39207"))))
- '(diff-removed ((t (:foreground "#cec9c9"))))
- ;; Error and warning messages
- '(error ((t (:foreground "#cec9c9"))))
- '(warning ((t (:foreground "#cec9c9"))))
- ;; Preprocessor directives
- '(font-lock-preprocessor-face ((t (:foreground "#cec9c9"))))
- ;; Constants and literals
- '(font-lock-constant-face ((t (:foreground "#f39207"))))
- ;; Keywords
- '(font-lock-keyword-face ((t (:foreground "#f39207"))))
- ;; Strings
- '(font-lock-string-face ((t (:foreground "#21eb10"))))
- ;; Functions
- '(font-lock-function-name-face ((t (:foreground "#f6f7f8"))))
+
+ ;; Syntax highlighting
+ `(default ((t (:foreground ,color_1 :background ,color_2))))
+ `(font-lock-comment-face ((t (:foreground ,color_5))))
+ `(font-lock-doc-face ((t (:foreground ,color_1))))
+ `(font-lock-string-face ((t (:foreground ,color_4))))
+ `(font-lock-keyword-face ((t (:foreground ,color_3))))
+ `(font-lock-function-name-face ((t (:foreground ,color_1))))
+ `(font-lock-variable-name-face ((t (:foreground ,color_1))))
+ `(font-lock-builtin-face ((t (:foreground ,color_1))))
+ `(font-lock-constant-face ((t (:foreground ,color_1))))
+ `(font-lock-type-face ((t (:foreground ,color_3))))
+ `(font-lock-preprocessor-face ((t (:foreground ,color_7))))
+
+ ;; Mode-line
+ `(mode-line ((t (:foreground ,color_8 :background ,color_9))))
+ `(mode-line-inactive ((t (:foreground ,color_8 :background ,color_9))))
+
+ ;; Highlighting current line
+ '(hl-line ((t (:background "#222222"))))
+
+ ;; Parenthesis matching
+ '(show-paren-match ((t (:background "#444444"))))
+ '(show-paren-mismatch ((t (:foreground "#FFFFFF" :background "#FF0000"))))
+
+ ;; Region selection
+ `(region ((t (:background "#444444"))))
+
+ ;; Minibuffer prompt
+ `(minibuffer-prompt ((t (:foreground ,color_6))))
+
  ;; Line numbers
- '(line-number ((t (:foreground "#4b4b4b" :background "#000000"))))
- ;; Comments
- '(font-lock-comment-face ((t (:foreground "#096dc1" :slant italic))))
- '(font-lock-comment-delimiter-face ((t (:foreground "#096dc1" :slant italic))))
- ;; Mode line
- '(mode-line ((t (:bold t :foreground "#b1b1b1" :background "#181717"))))
- '(mode-line-inactive ((t (:foreground "#b1b1b1" :background "#000000"))))
- ;; Search highlight
- '(isearch ((t (:background "#6a6a6d" :foreground "#b1b1b1"))))
- ;; Vertical split
- '(vertical-border ((t (:foreground "#181717"))))
- ;; Search match
- '(match ((t (:background "#540408"))))
- ;; Selection
- (set-face-attribute 'region nil :background "#540408")
- )
+ `(line-number ((t (:foreground ,color_3))))
+ `(line-number-current-line ((t (:foreground ,color_1))))
+
+ ;; Dired
+ `(dired-directory ((t (:foreground ,color_7))))
+ `(dired-flagged ((t (:foreground ,color_8))))
+ `(dired-header ((t (:foreground ,color_1))))
+ `(dired-ignored ((t (:foreground ,color_2))))
+ `(dired-marked ((t (:foreground ,color_7))))
+ `(dired-perm-write ((t (:foreground ,color_4))))
+ `(dired-symlink ((t (:foreground ,color_5))))
+ `(dired-warning ((t (:foreground ,color_3))))
+
+
+ ))
 
 (provide-theme 'loveisanillusion.emacs)
-
