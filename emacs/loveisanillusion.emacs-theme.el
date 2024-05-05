16b043fb4e3547f8c73bbe7c498dec8cbe176db6
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-05 07:55:00 -0700

    Emacs: Theme stuff

--- emacs/loveisanillusion.emacs-theme.el
+++ emacs/loveisanillusion.emacs-theme.el
@@ -1,6 +1,20 @@
 
 (deftheme loveisanillusion.emacs "Custom Emacs Theme")
 
+;; Defining Colors
+(let (
+      (color_1 "#c6c6c6")
+      (color_2 "#999999")
+      (color_3 "#151515")
+      (color_4 "#000000")
+      (color_5 "#ff9900")
+      (color_6 "#26d726")
+      (color_7 "#00acff")
+      (color_8 "#540408")
+      (color_9 "#f05624")
+      (color_10 "#0e0e0e")
+      (color_11 "#aa0000")
+  )
 
 ;; Startup Screen
 (setq inhibit-startup-message t)
@@ -15,8 +29,6 @@
 (defun maximize-frame ()
   "Maximizes the active frame in Windows"
   (interactive)
-  ;; Send a `WM_SYSCOMMAND' message to the active frame with the
-  ;; `SC_MAXIMIZE' parameter.
   (when (eq system-type 'windows-nt)
     (w32-send-sys-command 61488)))
 (add-hook 'window-setup-hook 'maximize-frame t)
@@ -41,7 +53,6 @@
       '((internal-border-width . 2)
         (left-fringe . 5)
         (right-fringe . 5)))
-
 (window-divider-mode)
 
 ;; Setting Fonts
@@ -51,62 +62,45 @@
                     :weight 'regular)
 
 
-
-
-(let ((normal_text_color "#cec9c9")
-      (black_color "#000000")
-      (brown_yellow_color "#f39207")
-      (light_green_color "#21eb10")
-      (blue_color "#00acff")
-      (maroon_color "#540408")
-      (dark_orange_color "#f05624")
-      (little_dark_normal_text_color "#b1b1b1")
-      (dark_gray_color "#181717")
-      )
-
 (custom-theme-set-faces
  'loveisanillusion.emacs
 
  ;; Syntax highlighting
- `(default ((t (:foreground ,normal_text_color :background ,black_color))))
- `(font-lock-comment-face ((t (:foreground ,blue_color))))
- `(font-lock-doc-face ((t (:foreground ,normal_text_color))))
- `(font-lock-string-face ((t (:foreground ,light_green_color))))
- `(font-lock-keyword-face ((t (:foreground ,brown_yellow_color))))
- `(font-lock-function-name-face ((t (:foreground ,normal_text_color))))
- `(font-lock-variable-name-face ((t (:foreground ,normal_text_color))))
- `(font-lock-builtin-face ((t (:foreground ,normal_text_color))))
- `(font-lock-constant-face ((t (:foreground ,normal_text_color))))
- `(font-lock-type-face ((t (:foreground ,brown_yellow_color))))
- `(font-lock-preprocessor-face ((t (:foreground ,dark_orange_color))))
+ `(default ((t (:foreground ,color_1 :background ,color_4))))
+ `(font-lock-comment-face ((t (:foreground ,color_7))))
+ `(font-lock-doc-face ((t (:foreground ,color_1))))
+ `(font-lock-string-face ((t (:foreground ,color_6))))
+ `(font-lock-keyword-face ((t (:foreground ,color_5))))
+ `(font-lock-function-name-face ((t (:foreground ,color_1))))
+ `(font-lock-variable-name-face ((t (:foreground ,color_1))))
+ `(font-lock-builtin-face ((t (:foreground ,color_1))))
+ `(font-lock-constant-face ((t (:foreground ,color_1))))
+ `(font-lock-type-face ((t (:foreground ,color_5))))
+ `(font-lock-preprocessor-face ((t (:foreground ,color_9))))
 
  ;; Mode-line
- `(mode-line ((t (:foreground ,little_dark_normal_text_color :background ,dark_gray_color))))
- `(mode-line-inactive ((t (:foreground ,little_dark_normal_text_color :background ,dark_gray_color))))
+ `(mode-line ((t (:foreground ,color_3 :background ,color_10))))
+ `(mode-line-inactive ((t (:foreground ,color_3 :background ,color_10))))
 
  ;; Parenthesis matching
- '(show-paren-match ((t (:background "#444444"))))
- '(show-paren-mismatch ((t (:foreground "#FFFFFF" :background "#FF0000"))))
+ `(show-paren-match ((t (:background ,color_2))))
+ `(show-paren-mismatch ((t (:foreground ,color_1 :background ,color_11))))
 
  ;; Region selection
- `(region ((t (:background ,maroon_color))))
+ `(region ((t (:background ,color_8))))
 
  ;; Minibuffer prompt
- `(minibuffer-prompt ((t (:foreground ,blue_color))))
-
- ;; Line numbers
- `(line-number ((t (:foreground ,brown_yellow_color))))
- `(line-number-current-line ((t (:foreground ,normal_text_color))))
+ `(minibuffer-prompt ((t (:foreground ,color_9))))
 
  ;; Dired
- `(dired-directory ((t (:foreground ,dark_orange_color))))
- `(dired-flagged ((t (:foreground ,little_dark_normal_text_color))))
- `(dired-header ((t (:foreground ,normal_text_color))))
- `(dired-ignored ((t (:foreground ,black_color))))
- `(dired-marked ((t (:foreground ,dark_orange_color))))
- `(dired-perm-write ((t (:foreground ,light_green_color))))
- `(dired-symlink ((t (:foreground ,blue_color))))
- `(dired-warning ((t (:foreground ,brown_yellow_color))))
+ `(dired-directory ((t (:foreground ,color_9))))
+ `(dired-flagged ((t (:foreground ,color_2))))
+ `(dired-header ((t (:foreground ,color_1))))
+ `(dired-ignored ((t (:foreground ,color_2))))
+ `(dired-marked ((t (:foreground ,color_9))))
+ `(dired-perm-write ((t (:foreground ,color_6))))
+ `(dired-symlink ((t (:foreground ,color_7))))
+ `(dired-warning ((t (:foreground ,color_11))))
 
 
  ))
