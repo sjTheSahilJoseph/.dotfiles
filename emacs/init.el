ae8dd0627d384558b9377d3964301de578e6baf1
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-06 03:23:19 -0700

    ...

--- emacs/init.el
+++ emacs/init.el
@@ -105,15 +105,17 @@
 
 
 ;; Package Stuff ... coming ...
-
-;; Configure package.el to include MELPA.
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-(defun package--save-selected-packages (&rest opt) nil)
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
+(use-package typescript-mode
+  :mode ("\\.tsx?\\'" . typescript-mode))
