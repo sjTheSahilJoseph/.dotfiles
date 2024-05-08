06b741faca5a3a442d35e395d14c75d64b68b66f
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-07 22:13:12 -0700

    Emacs: Packages Setup

--- emacs/init.el
+++ emacs/init.el
@@ -134,3 +134,15 @@
   :ensure t
   :defer t)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(json-mode rainbow-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
