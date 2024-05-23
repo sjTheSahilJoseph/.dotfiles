770379653382d96d99237d2f4a968d12a01832e6
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-23 23:38:37 +0500

    Emacs: Some Cleaning

--- emacs/init.el
+++ emacs/init.el
@@ -281,30 +281,3 @@
 		`(org-level-2 ((t (,@headline ,@variable-tuple :height 1.3))))
 		`(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
 		`(org-document-title ((t (,@headline ,@variable-tuple :height 1.6 :underline t))))))
-
-
-
-
-
-(custom-set-variables
-	;; custom-set-variables was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
-	'(package-selected-packages '(json-mode rainbow-mode)))
-(custom-set-faces
-	;; custom-set-faces was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
-	'(org-document-title ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.6 :underline t))))
-	'(org-level-1 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.5))))
-	'(org-level-2 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.3))))
-	'(org-level-3 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.2))))
-	'(org-level-4 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.1))))
-	'(org-level-5 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas"))))
-	'(org-level-6 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas"))))
-	0 '(org-level-7 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas"))))
-	'(org-level-8 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas")))))
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
