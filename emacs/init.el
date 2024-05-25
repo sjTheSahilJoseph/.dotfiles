6f0c66be8a7a3d1158e0a5df34f10fdeee100d3c
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-25 22:07:23 +0500

    Emacs: Kotlin Support

--- emacs/init.el
+++ emacs/init.el
@@ -210,6 +210,11 @@
 	:ensure t
 	:defer t)
 
+(use-package kotlin-mode
+	:ensure t
+	:defer t)
+
+
 ;; Indent
 (setq electric-indent-mode t)
 (setq-default indent-tabs-mode t)
@@ -296,3 +301,23 @@
 		`(org-level-2 ((t (,@headline ,@variable-tuple :height 1.3))))
 		`(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
 		`(org-document-title ((t (,@headline ,@variable-tuple :height 1.6 :underline t))))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(kotlin-mode typescript-mode python-mode json-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.6 :underline t))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.5))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.3))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.2))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#c6c6c6" :font "Consolas")))))
