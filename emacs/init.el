404fe86379be6d616908908577a3bd9e9b47b6be
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-06-08 20:11:40 +0500

    emacs added some stuff to the file

--- emacs/init.el
+++ emacs/init.el
@@ -267,3 +267,15 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(yasnippet typescript-mode json-mode csharp-mode corfu)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
