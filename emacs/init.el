3876083948bc55451abe766a0adee35f03bcfd33
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-07-18 17:48:53 +0500

    ...

--- emacs/init.el
+++ emacs/init.el
@@ -163,6 +163,10 @@
 	:ensure t
 	:defer t)
 
+(use-package rainbow-mode
+	:ensure t
+	:defer t)
+
 
 ;; Indent
 (setq electric-indent-mode t)
@@ -206,3 +210,16 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	  '(rainbow-mode yasnippet web-mode typescript-mode python-mode php-mode json-mode emmet-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
