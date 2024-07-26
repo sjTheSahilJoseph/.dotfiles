1ea54b3197f6abc98bcbecff98f435efb40c55c5
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-07-26 11:07:41 +0500

    ...'

--- emacs/init.el
+++ emacs/init.el
@@ -164,8 +164,8 @@
 	:defer t)
 
 (use-package rainbow-mode
-  :ensure t
-  :hook (prog-mode . rainbow-mode))
+	:ensure t
+	:hook (prog-mode . rainbow-mode))
 
 
 ;; Indent
@@ -211,15 +211,15 @@
 
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-	  '(rainbow-mode yasnippet web-mode typescript-mode python-mode php-mode json-mode emmet-mode)))
+	;; custom-set-variables was added by Custom.
+	;; If you edit it by hand, you could mess it up, so be careful.
+	;; Your init file should contain only one such instance.
+	;; If there is more than one, they won't work right.
+	'(package-selected-packages
+		 '(rainbow-mode yasnippet web-mode typescript-mode python-mode php-mode json-mode emmet-mode)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+	;; custom-set-faces was added by Custom.
+	;; If you edit it by hand, you could mess it up, so be careful.
+	;; Your init file should contain only one such instance.
+	;; If there is more than one, they won't work right.
+	)
