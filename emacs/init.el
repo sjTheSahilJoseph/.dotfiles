bfa4efefdf7483b616e474fa2fc2e3271ceb1e89
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-07-27 21:16:27 +0500

    cursor color change

--- emacs/init.el
+++ emacs/init.el
@@ -24,7 +24,7 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (setq-default cursor-type 'box)
-(set-cursor-color "#f3f4f6")
+(set-cursor-color "#000000")
 (blink-cursor-mode t)
 (setq blink-cursor-blinks 0)
 (setq blink-cursor-interval 0.5)
