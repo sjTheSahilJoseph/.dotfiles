8699a1026abdc1e93d84f32350dac645ee77610b
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-09-06 23:09:06 +0500

    undotree

--- emacs/init.el
+++ emacs/init.el
@@ -143,6 +143,13 @@
          )
   :commands lsp)
 
+;; Undo Tree
+(use-package undo-tree
+  :ensure t
+  :defer t
+  )
+(global-undo-tree-mode)
+
 ;; Language Modes
 (use-package json-mode
   :ensure t
@@ -223,17 +230,3 @@
 (put 'upcase-region 'disabled nil)
 
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(which-key typescript-mode treemacs-tab-bar treemacs-projectile treemacs-icons-dired rust-mode rainbow-mode python-mode lua-mode lsp-ui lsp-treemacs lsp-ivy json-mode helm-lsp flycheck company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
