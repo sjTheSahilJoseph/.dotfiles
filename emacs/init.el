ab921bd6857baef6cccc1430de9f69263a3cc576
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-09-06 22:39:35 +0500

    corfu is working

--- emacs/init.el
+++ emacs/init.el
@@ -136,6 +136,7 @@
 		 (rust-mode . lsp)
 		 (python-mode . lsp)
 		 (cc-mode . lsp)
+		 (c-mode . lsp)
 		 (csharp-mode . lsp)
 		 (json-mode . lsp)
 		 (lua-mode . lsp)
@@ -172,6 +173,31 @@
 (use-package lua-mode
   :ensure t
   :defer t)
+
+;; Completion
+(use-package corfu
+ :custom
+ (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+ (corfu-auto t)                 ;; Enable auto completion
+ (corfu-separator ?\s)          ;; Orderless field separator
+ (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+ (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+ (corfu-preview-current nil)    ;; Disable current candidate preview
+ (corfu-preselect 'prompt)      ;; Preselect the prompt
+ (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+ (corfu-scroll-margin 5)        ;; Use scroll margin
+ Enable Corfu only for certain modes. See also `global-corfu-modes'.
+ :hook ((prog-mode . corfu-mode)
+        (shell-mode . corfu-mode)
+        (eshell-mode . corfu-mode))
+  :init
+  (global-corfu-mode))
+
+(use-package emacs
+  :custom
+  (tab-always-indent 'complete)
+  (text-mode-ispell-word-completion nil)
+  (read-extended-command-predicate #'command-completion-default-include-p))
 
 ;; Rainbow Mode
 (use-package rainbow-mode
