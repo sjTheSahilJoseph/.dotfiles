ad36d617dfd78bf7f0b1a5eb96f71f8301a99868
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-09-10 20:25:19 +0500

    font size -> 130 -> 120 -> 130

--- emacs/init.el
+++ emacs/init.el
@@ -238,17 +238,3 @@
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(neotree which-key undo-tree typescript-mode treemacs-tab-bar treemacs-projectile treemacs-icons-dired rust-mode rainbow-mode python-mode lua-mode lsp-ui lsp-treemacs lsp-ivy json-mode helm-lsp flycheck corfu company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
