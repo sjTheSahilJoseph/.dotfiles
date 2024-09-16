c68e433b39cfe8d2ed80ba83d2c9f189d946b74a
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-09-17 03:24:22 +0500

    emacs: straight config

--- emacs/init.el
+++ emacs/init.el
@@ -246,6 +246,28 @@
                   ("M-n" . flycheck-next-error)
                   ("M-p" . flycheck-previous-error)))
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+(straight-use-package 'use-package)
+
+(straight-use-package '(tsx-mode :type git :host github :repo "orzechowskid/tsx-mode.el"))
+
+
 
 ;; Indent
 (setq electric-indent-mode t)
