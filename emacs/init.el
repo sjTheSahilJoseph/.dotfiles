daf5a1d0793e9903ebc283c688eb984db85c39cd
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-09-24 23:25:32 +0500

    ...

--- emacs/init.el
+++ emacs/init.el
@@ -192,16 +192,3 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(php-mode rainbow-mode lua-mode python-mode rust-mode emmet-mode typescript-mode json-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
