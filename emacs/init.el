ab6baf1b55f5cb80e9f442e3cb9aa2ab147b0868
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-10-04 01:42:27 +0500

    ...

--- emacs/init.el
+++ emacs/init.el
@@ -241,16 +241,4 @@
 (put 'upcase-region 'disabled nil)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(which-key web-mode typescript-mode rust-mode rainbow-mode python-mode prettier-js php-mode npm-mode lua-mode lsp-ui lsp-python-ms json-mode emmet-mode editorconfig company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
