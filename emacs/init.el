71716d5218a9726f1cefcae372219798c50d4b71
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-10-06 16:13:49 +0500

    ..

--- emacs/init.el
+++ emacs/init.el
@@ -233,3 +233,16 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+      '(tree-sitter treesit-auto jtsx which-key web-mode typescript-mode tide rust-mode rainbow-mode python-mode prettier-js php-mode npm-mode lua-mode lsp-ui lsp-python-ms json-mode emmet-mode editorconfig corfu company aggressive-indent)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
