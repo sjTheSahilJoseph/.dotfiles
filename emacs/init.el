ca0dbaec2396fa742dd5832468e2edf3534c7636
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-10-17 16:58:50 +0500

    indentation tab 4

--- emacs/init.el
+++ emacs/init.el
@@ -137,8 +137,7 @@
 (use-package typescript-mode
 	:ensure t
 	:defer t
-	:config
-	(setq typescript-indent-level 4))
+    )
 
 (use-package php-mode
 	:ensure t
@@ -148,8 +147,6 @@
 (use-package cc-mode
 	:ensure t
 	:defer t
-	:config
-	(setq c-basic-offset 4)
 	)
 
 (use-package rust-mode
@@ -170,7 +167,9 @@
 (setq electric-indent-mode 1)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(setq c-basic-offset 4)
 (setq lisp-indent-offset 4)
+(setq typescript-indent-level 4)
 
 (defun indent-whole-buffer ()
 	"Indent the entire buffer."
@@ -186,34 +185,34 @@
 
 
 (defun lsp-booster--advice-json-parse (old-fn &rest args)
-  "Try to parse bytecode instead of json."
-  (or
-   (when (equal (following-char) ?#)
-     (let ((bytecode (read (current-buffer))))
-       (when (byte-code-function-p bytecode)
-         (funcall bytecode))))
-   (apply old-fn args)))
+    "Try to parse bytecode instead of json."
+    (or
+        (when (equal (following-char) ?#)
+            (let ((bytecode (read (current-buffer))))
+                (when (byte-code-function-p bytecode)
+                    (funcall bytecode))))
+        (apply old-fn args)))
 (advice-add (if (progn (require 'json)
-                       (fboundp 'json-parse-buffer))
+                    (fboundp 'json-parse-buffer))
                 'json-parse-buffer
-              'json-read)
-            :around
-            #'lsp-booster--advice-json-parse)
+                'json-read)
+    :around
+    #'lsp-booster--advice-json-parse)
 
 (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-  "Prepend emacs-lsp-booster command to lsp CMD."
-  (let ((orig-result (funcall old-fn cmd test?)))
-    (if (and (not test?)                             ;; for check lsp-server-present?
-             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-             lsp-use-plists
-             (not (functionp 'json-rpc-connection))  ;; native json-rpc
-             (executable-find "emacs-lsp-booster"))
-        (progn
-          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
-            (setcar orig-result command-from-exec-path))
-          (message "Using emacs-lsp-booster for %s!" orig-result)
-          (cons "emacs-lsp-booster" orig-result))
-      orig-result)))
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+        (if (and (not test?)                             ;; for check lsp-server-present?
+                (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+                lsp-use-plists
+                (not (functionp 'json-rpc-connection))  ;; native json-rpc
+                (executable-find "emacs-lsp-booster"))
+            (progn
+                (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+                    (setcar orig-result command-from-exec-path))
+                (message "Using emacs-lsp-booster for %s!" orig-result)
+                (cons "emacs-lsp-booster" orig-result))
+            orig-result)))
 (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (setq eldoc-echo-area-use-multiline-p nil)
