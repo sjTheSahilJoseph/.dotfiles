59e0fdc8634a44487a2e21c35d15f74de74490f2
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-10-20 22:12:49 +0500

    ..

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,10 +1,7 @@
 
 ;; Memory ++
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'after-init-hook
-    (lambda ()
-        (setq gc-cons-threshold (* 200 1024 1024))))
-
 (setq read-process-output-max (* 200 1024 1024))
+(setq gc-cons-threshold 1073741824
+      gc-cons-percentage 0.6)
 
 (provide 'early-init)
