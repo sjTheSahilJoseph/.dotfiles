ef4a1121d6ac78b3072d4df3cfd0ab2e901ac540
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2025-05-11 10:22:37 +0500

    ...

--- emacs/init.el
+++ emacs/init.el
@@ -360,9 +360,9 @@
     )
 
 (use-package orderless
+    :ensure t
     :custom
     (completion-styles '(orderless basic))
     (completion-category-defaults nil)
     (completion-category-overrides '((file (styles partial-completion)))))
-
 
