ccc161e0994344d59cd3d183730718b7a127a02f
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2025-09-25 00:26:27 +0500

    f8 -> eval region

--- emacs/init.el
+++ emacs/init.el
@@ -287,7 +287,7 @@
 (global-set-key (kbd "<f5>") 'run-build-script)
 (global-set-key (kbd "<f6>") 'hl-line-mode)
 (global-set-key (kbd "<f7>") 'query-replace)
-(global-set-key (kbd "<f8>") 'dired)
+(global-set-key (kbd "<f8>") 'eval-region)
 (global-set-key (kbd "<f9>") #'visual-line-mode)
 (global-set-key (kbd "<f10>") 'indent-whole-buffer)
 (global-set-key (kbd "<f11>") 'toggle-frame-fullscreen)
@@ -301,16 +301,3 @@
 
 
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
