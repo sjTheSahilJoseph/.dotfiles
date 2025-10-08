9970225cd80599fcc7a56ceeccf7abaf4c340324
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2025-10-08 13:08:41 +0500

    don't split automatically

--- emacs/init.el
+++ emacs/init.el
@@ -13,7 +13,7 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(split-window-horizontally)
+;;(split-window-horizontally)
 
 (setq-default cursor-type 'bar)
 
@@ -308,3 +308,15 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
